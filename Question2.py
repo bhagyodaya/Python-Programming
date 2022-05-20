48a0afaebe401292c65150c753bfe5faf497962d
Bhagyodaya Varshney <92192554+bhagyodaya@users.noreply.github.com>
2022-05-20 17:49:34 +0530

    Add files via upload

---
+++ Question2.py
@@ -0,0 +1,12 @@
+#A man has certain numbers of apples.
+#if he picks them in grp of 7,he can pick all of them
+#if he picks 
+
+
+
+num=1
+while 1:
+    if num%7==0 and num%6==1 and num%5==1 and num%4==1 and num%3==1 and num%2==1:
+        print(num)
+    break
+num +=1
