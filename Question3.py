c92570cecd522b03cb23b25e2716014cfd0bc9c7
Bhagyodaya Varshney <92192554+bhagyodaya@users.noreply.github.com>
2022-05-20 17:50:04 +0530

    Add files via upload

---
+++ Question3.py
@@ -0,0 +1,103 @@
+date=int(input("Enter the date:"))
+month=int(input("Enter the month:"))
+year=int(input("Enter the year:"))
+
+if year%4==0:
+    
+ if month==1:
+    a=31-date
+    b=335+a
+    print(b)
+ elif month==2:
+    a=28-date
+    b=307+a
+    print(b)
+ elif month==3:
+    a=31-date
+    b=276+a
+    print(b)
+ elif month==4:
+    a=30-date
+    b=246+a
+    print(b)
+ elif month==5:
+    a=31-date
+    b=215+a
+    print(b)
+ elif month==6:
+    a=30-date
+    b=185+a
+    print(b)
+ elif month==7:
+    a=31-date
+    b=154+a
+    print(b)
+ elif month==8:
+    a=31-date
+    b=123+a
+    print(b)
+ elif month==9:
+    a=30-date
+    b=93+a
+    print(b)
+ elif month==10:
+    a=31-date
+    b=62+a
+    print(b)
+ elif month==11:
+    a=30-date
+    b=32+a
+    print(b)
+ elif month==12:
+    a=32-date
+    print(a)
+
+else:
+    
+ if month==1:
+    a=31-date
+    b=334+a
+    print(b)
+ elif month==2:
+    a=28-date
+    b=306+a
+    print(b)
+ elif month==3:
+    a=31-date
+    b=275+a
+    print(b)
+ elif month==4:
+    a=30-date
+    b=245+a
+    print(b)
+ elif month==5:
+    a=31-date
+    b=214+a
+    print(b)
+ elif month==6:
+    a=30-date
+    b=184+a
+    print(b)
+ elif month==7:
+    a=31-date
+    b=153+a
+    print(b)
+ elif month==8:
+    a=31-date
+    b=122+a
+    print(b)
+ elif month==9:
+    a=30-date
+    b=92+a
+    print(b)
+ elif month==10:
+    a=31-date
+    b=61+a
+    print(b)
+ elif month==11:
+    a=30-date
+    b=31+a
+    print(b)
+ elif month==12:
+    a=31-date
+    print(a)
