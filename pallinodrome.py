f37fda317e49707d7eaede904018d02f0a3a1bcd
Bhagyodaya Varshney <92192554+bhagyodaya@users.noreply.github.com>
2022-05-25 06:19:34 +0530

    Add files via upload

---
+++ pallinodrome.py
@@ -0,0 +1,16 @@
+from audioop import reverse
+from re import X
+
+
+n=int(input("Enter No:"))
+r=0
+x=n
+while n>0:
+    s=n%10
+    r=r*10+s
+    n //=10
+print(r)
+if x==r:
+    print("it is a plainodrome Number")
+
+
