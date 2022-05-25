682ca25684c09a13f7aa0d303ba7ff8593da5929
Bhagyodaya Varshney <92192554+bhagyodaya@users.noreply.github.com>
2022-05-25 06:30:32 +0530

    Add files via upload

---
+++ Dict1.py
@@ -0,0 +1,32 @@
+idl=["2115000284","2115000290","2115000285"]
+#phone_numberl=["6398693679","8266010229","8266010239"]
+#cPassl=["1234","5678","9101"]
+#userNl=["Bhagyodaya Varshney","Bhav Agrawal","Gauri varshney"]
+#print("__________________ Welcome To GLA Portal______________________")
+#print("For Student Type 'S'")
+#print("For Admin Type'A'")
+#own=input("Enter Status 'Student or Admin':")
+#if own == "S":
+ #   print("*****************Welcome To Student Portal*******************")
+
+#elif own == "A":
+ #   print("*******************Welcome to Admin Portal*******************")
+  #  id=input("Enter Your ID:")
+   # Pass=input("Enter Your ID Password:")
+    #user=input("Enter Your USer NAme:")
+    #if id in idl:
+     #   if Pass in cPassl:
+      #      if user  in userNl:
+       #             print("Welcome ",user,"To Your Portal")
+        #    else:
+         #       print("Please Enter Correct User Name ")
+        #else:
+         #       print("Please Enter Correct Password")
+    #else:
+     #       print("Please Enter Correct Id ")
+#else:
+ #   print("Please Enter 'A' for Admin and 'S' for Student")
+n=input("Enter ID:")
+idl.append(n)
+if "2115000240" in idl:
+    print(idl)
