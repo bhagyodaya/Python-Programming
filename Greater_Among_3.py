d22230735f84d904967cefb0007bc83e8be9c280
Bhagyodaya Varshney <92192554+bhagyodaya@users.noreply.github.com>
2022-05-27 20:27:55 +0530

    Add files via upload

---
+++ Greater_Among_3.py
@@ -0,0 +1,9 @@
+num1=int(input("Enter 1 number:"))
+num2=int(input("Enter 2 number:"))
+num3=int(input("enter 3 number:"))
+if num1>num2 and num1>num3:
+    print("Number1 enter is greater")
+elif num2>num1 and num2>num3:
+    print("Number 2 enter is greater")
+else:
+    print("Nuber 3 entered is greater")
